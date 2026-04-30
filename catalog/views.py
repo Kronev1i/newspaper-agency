a7fe664785b1ab8f17633000ac7120a1d5344d17
unknown <kgoncharenko83@gmail.com>
2026-04-30 17:59:33 +0200

    feat: add list views for Topic, Newspaper & Redactor

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,55 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.views import generic
+from .models import Redactor, Newspaper, Topic
 
-# Create your views here.
+
+@login_required
+def index(request):
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+    num_topics = Topic.objects.count()
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1,
+    }
+
+    return render(
+        request,
+        "newspaper/index.html",
+        context=context
+    )
+
+
+class TopicsListView(
+    LoginRequiredMixin,
+    generic.ListView
+):
+    model = Topic
+    context_object_name = "topic_list"
+    template_name = "newspaper/topic_list.html"
+    paginate_by = 5
+
+
+class CarListView(
+    LoginRequiredMixin,
+    generic.ListView
+):
+    model = Newspaper
+    paginate_by = 5
+    queryset = Newspaper.objects.all().select_related("topic")
+
+
+class RedactorListView(
+    LoginRequiredMixin,
+    generic.ListView
+):
+    model = Redactor
+    paginate_by = 5
