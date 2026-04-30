c2d9074545b195359561b6424d09ce1a32f6413d
unknown <kgoncharenko83@gmail.com>
2026-04-30 17:39:56 +0200

    feat: add models to admin site

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import Redactor, Topic, Newspaper
 
-# Register your models here.
+
+@admin.register(Redactor)
+class RedactorAdmin(admin.ModelAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Additional info", {"fields": ("years_of_experience",)}),
+    )
+
+    admin.site.register(Newspaper)
+    admin.site.register(Topic)
