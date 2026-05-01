d60685e37d149f71ee0642f5b42ccb1105b8541d
unknown <kgoncharenko83@gmail.com>
2026-05-01 20:37:41 +0200

    feat: add validation for years of experience

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
+from django.core.exceptions import ValidationError
+
 from catalog.models import Redactor, Newspaper
 
 
@@ -35,3 +37,15 @@ class NewspaperTitleSearchForm(forms.Form):
 
 class TopicNameSearchForm(forms.Form):
     name = forms.CharField(max_length=255, required=False)
+
+
+class RedactorExperienceUpdateForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ["years_of_experience"]
+
+    def clean_years_of_experience(self):
+        years = self.cleaned_data["years_of_experience"]
+        if not (0 <= years <= 70):
+            raise ValidationError("Experience should be between 0 and 70 years.")
+        return years
