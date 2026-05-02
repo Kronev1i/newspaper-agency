8d6d08ef096175519042b788c1f94bac8e7e1e41
unknown <kgoncharenko83@gmail.com>
2026-05-02 21:29:11 +0200

    feat: sqlite base > postgresql base for deploying

---
+++ newspaper_agency/settings/dev.py
@@ -0,0 +1,15 @@
+from .base import *
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+
+# Database
+# https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
